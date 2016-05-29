22662cc9835aeac272f225448696334f22c711b3
Pablo Garcia <pgarciacamou@gmail.com>
2016-05-28 23:02:42 -0700

    [bugfix] preprocessing src files with browserify as well

--- karma.conf.js
+++ karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': [ 'browserify' ],
       'specs/**/*.spec.js': [ 'browserify' ]
     },
 
