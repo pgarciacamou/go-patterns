d6dff434cb03951626c0fbcb47b048bd93feb25a
Pablo Garcia <pgarciacamou@gmail.com>
2016-05-29 22:26:56 -0700

    added comments to observer pattern

--- src/patterns/behavioral/observer.js
+++ src/patterns/behavioral/observer.js
@@ -1,3 +1,34 @@
+/**
+ * @class
+ *
+ * A way of notifying change to a number of classes to ensure consistency between the classes.
+ *
+ * @param {Function} update which will be executed when notified.
+ * @param {Subject} subject which allows to have access to the subject that holds the observer.
+ */
+class Observer {
+  constructor(update, subject) {
+    this.update = update;
+    this.subject = subject;
+  }
+
+  /**
+   * @method
+   * @public
+   * 
+   * removes the obsever from the subject's list.
+   */
+  destroy() {
+    this.subject.removeObserver(this);
+  }
+}
+
+/**
+ * @class
+ * @private
+ *
+ * creates an instance that will manage the list of observers.
+ */
 class ObserverList {
   constructor() {
     this._list = [];
@@ -19,6 +50,12 @@ class ObserverList {
   }
 }
 
+/**
+ * @class
+ * @private
+ *
+ * allows observers to subscribe to a subject (instance of Subject).
+ */
 class Subject {
   constructor(name) {
     this.name = name;
@@ -38,23 +75,35 @@ class Subject {
   }
 }
 
-class Observer {
-  constructor(fn, subject) {
-    this.update = fn;
-    this.subject = subject;
-  }
-  destroy() {
-    this.subject.removeObserver(this);
-  }
-}
 
 var subjects = {};
+
+/**
+ * @method
+ * @public
+ *
+ * @param {String} subject name
+ * @param {Function} update callback
+ *
+ * @return {Observer} obs instance that allows destruction
+ */
 export default function observer(subject, update) {
   var sub = subjects[subject] = subjects[subject] || new Subject(subject);
   var obs = new Observer(update, sub);
   sub.addObserver(obs);
   return obs;
 }
+
+/**
+ * @method
+ * @public
+ * @static
+ *
+ * will trigger the subject to notify the observers.
+ *
+ * @param {String} subject name
+ * @param {Object} data sent to observers
+ */
 observer.notify = function(subject, data) {
   var sub = subjects[subject];
   if(sub) {
