93562fe8ab7a3a665a4990fffe61b85ff14e4288
Pablo Garcia <pgarciacamou@gmail.com>
2016-06-22 22:15:35 -0700

    cleaned factory

--- src/patterns/creational/factory.js
+++ src/patterns/creational/factory.js
@@ -1,6 +1,13 @@
 import extend from "../../helpers/extend.js";
 import recursiveInheritance from "../../helpers/recursive-inheritance.js";
 
+/**
+ * A Factory Pattern: makes an instance of several
+ * derived classes based on interfaced data or events.
+ * @method
+ * @param  {object} commonFunctionality which will be inherited from the classes
+ * @return {Factory} factory instance
+ */
 function factory(options) {
   options = extend({
     constructor: function () {},
@@ -53,69 +60,5 @@ function factory(options) {
     }
   };
 }
-
-// /**
-//  * A Factory Pattern: makes an instance of several
-//  * derived classes based on interfaced data or events.
-//  * @method
-//  * @param  {object} commonFunctionality which will be inherited from the classes
-//  * @return {Factory} factory instance
-//  */
-// function factory(__superclass) {
-//   /**
-//    * @class
-//    */
-//   class Factory {
-//     /**
-//      * @param  {Object} commonFunctionality
-//      */
-//     constructor (...args) {
-//       this.__superclassArgs = args;
-//       this.__classes = {};
-//     }
-
-//     /**
-//      * @param {String} className
-//      * @param {Class} __class
-//      */
-//     add(className, __class) {
-//       if(this.__classes[className] !== undefined) {
-//         throw new Error("class is already defined in factory");
-//       }
-
-//       // __class.prototype = extend(
-//       //   Object.create(__superclass.prototype),
-//       //   { constructor: __class },
-//       //   __class.prototype
-//       // );
-
-//       // this.__classes[className] = (...args) => {
-//       //   var instance = Object.create(__class.prototype);
-//       //   __class.apply(instance, args);
-//       //   __superclass.apply(instance, supperArgs);
-//       //   return instance;
-//       // };
-
-//       this.__classes[className] = recursiveInheritance(__class, __superclass);
-//       return this;
-//     }
-
-//     /**
-//      * @param  {String} className
-//      * @param  {Array} args
-//      * @return {Object}
-//      */
-//     create(className, ...args){
-//       if(this.__classes[className] === undefined) {
-//         throw new Error("class is not defined in factory");
-//       }
-
-//       // class is dynamic instance of RecursiveInheritance.
-//       return new this.__classes[className](args, this.__superclassArgs);
-//     }
-//   }
-
-//   return Factory;
-// }
 
 export default factory;
