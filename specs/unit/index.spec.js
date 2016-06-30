19e4e57b74f43617cb277627e07780e794596cf8
Pablo Garcia <pgarciacamou@gmail.com>
2016-06-30 09:35:57 -0700

    [bugfix] missing tests for new pattern

--- specs/unit/index.spec.js
+++ specs/unit/index.spec.js
@@ -2,11 +2,12 @@ import patterns from '../../src/index.js'
 
 describe('patterns API', function() {
   it('should describe an API', function() {
-    expect(Object.keys(patterns).length).toEqual(3);
+    expect(Object.keys(patterns).length).toEqual(4);
   });
   it('should contain patterns', function() {
     expect(patterns.singleton).toBeDefined();
     expect(patterns.factory).toBeDefined();
     expect(patterns.publishSubscribe).toBeDefined();
+    expect(patterns.chainOfResponsibility).toBeDefined();
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import singleton from './patterns/creational/singleton.js';
 import factory from './patterns/creational/factory.js';
 import publishSubscribe from './patterns/behavioral/publishSubscribe.js';
+import chainOfResponsibility from './patterns/behavioral/chainOfResponsibility.js';
 
 var patterns = {
   singleton,
   factory,
-  publishSubscribe
+  publishSubscribe,
+  chainOfResponsibility
 };
 
 export default patterns;
