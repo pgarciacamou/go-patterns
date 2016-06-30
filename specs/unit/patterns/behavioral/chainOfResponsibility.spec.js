09947700f5bda03d01ce96446e029bf4af6756db
Pablo Garcia <pgarciacamou@gmail.com>
2016-06-29 20:26:39 -0700

    chainOfResponsibility pattern finished

--- specs/unit/patterns/behavioral/chainOfResponsibility.spec.js
+++ specs/unit/patterns/behavioral/chainOfResponsibility.spec.js
@@ -4,6 +4,8 @@ describe('chain of responsibility', function() {
   var arraySpy;
   var functionSpy;
   var constructorSpy;
+  var shouldNotRunSpy;
+  var defaultSpy;
   var Pattern;
   var patternImplementation;
   var chain;
@@ -11,7 +13,9 @@ describe('chain of responsibility', function() {
     constructorSpy = jasmine.createSpy("constructor");
     arraySpy = jasmine.createSpy("array");
     functionSpy = jasmine.createSpy("object");
-    Pattern = buildPattern({
+    shouldNotRunSpy = jasmine.createSpy("shouldNotRun");
+    defaultSpy = jasmine.createSpy("default");
+    Pattern = chainOfResponsibility({
       constructor: _ => {
         constructorSpy();
       },
@@ -21,13 +25,11 @@ describe('chain of responsibility', function() {
     });
     patternImplementation = Pattern.build();
     chain = new patternImplementation()
-    .add((next, t) => {
-      return "default";
-    })
+    .add(defaultSpy)
     .add((next, t) => {
       if(t instanceof Array) {
         arraySpy();
-        return t;
+        return "array";
       }
       next();
     })
@@ -41,7 +43,14 @@ describe('chain of responsibility', function() {
     .add((next, t) => {
       if(t instanceof Function) {
         functionSpy();
-        return t;
+        return "function";
+      }
+      next();
+    })
+    .add((next, t) => {
+      if(t === "continue") {
+        next();
+        return "shouldNotReturnThis";
       }
       next();
     });
@@ -51,23 +60,34 @@ describe('chain of responsibility', function() {
     expect(constructorSpy).toHaveBeenCalled();
   });
   it('should chain callbacks', function() {
-    expect(chain.run()).toEqual("default");
-
+    chain.run();
+    expect(chain.run([])).toEqual("array");
+    expect(chain.run(_ => {})).toEqual("function");
+    expect(defaultSpy).toHaveBeenCalled();
+    expect(functionSpy).toHaveBeenCalled();
+    expect(arraySpy).toHaveBeenCalled();
   });
   it('can shadow callbacks', function() {
-    
+    expect(chain.run(_ => {})).not.toEqual("shouldNotRun");
+    expect(shouldNotRunSpy).not.toHaveBeenCalled();
   });
   it('can continue the chain even if callback was used', function() {
-    
+    var t = chain.run("continue")
+    expect(t).not.toEqual("shouldNotReturnThis");
+    expect(defaultSpy).toHaveBeenCalled();
   });
-  it('should run regardless of the context', function() {
-    
+  it('should run regardless of the context allowing to pass as callback', function(done) {
+    setTimeout(chain.run);
+    setTimeout(_ => {
+      expect(defaultSpy).toHaveBeenCalled();
+      done();
+    }, 30);
   });
   it('should count the callbacks', function() {
-    
+    expect(chain.count()).toEqual(5);
   });
   it('should find callbacks', function() {
-    
+    expect(chain.contains(defaultSpy)).toBeTruthy();
+    expect(chain.contains(_ => {})).toBeFalsy();
   });
-
 });
