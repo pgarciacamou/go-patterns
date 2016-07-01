675239f1307732ced5c60c35a634c8a94b7467b6
Pablo Garcia <pgarciacamou@gmail.com>
2016-06-30 22:22:46 -0700

    finished mediator

---
+++ specs/unit/patterns/behavioral/mediator.spec.js
@@ -0,0 +1,65 @@
+import mediator from "../../../../src/patterns/behavioral/mediator.js";
+
+fdescribe('mediator', function() {
+  var participantA;
+  var participantB;
+  var participantC;
+  var constructor;
+  var Pattern;
+  var patternImplementation;
+
+  beforeEach(function() {
+    participantA = jasmine.createSpy("A");
+    participantB = jasmine.createSpy("B");
+    participantC = jasmine.createSpy("B");
+    constructor = jasmine.createSpy("constructor");
+    Pattern = mediator({
+      constructor: function (...args) {
+        constructor(...args);
+      },
+      publics: {
+        test: "testing"
+      }
+    }).build();
+    patternImplementation = new Pattern("test");
+    patternImplementation.register("participantA", participantA);
+    patternImplementation.register("participantB", participantB);
+    patternImplementation.send("testA", "participantB", "participantA");
+    patternImplementation.send("testB", "participantA", "participantB");
+    patternImplementation.broadcast("broadcast", "participantA");
+    patternImplementation.send("send broadcast", "participantA");
+    patternImplementation.send("anonymous", undefined, "participantA");
+    patternImplementation.send("send anonymous broadcast");
+    patternImplementation.broadcast("anonymous broadcast");
+    patternImplementation.send("lost", "participantA", "non-existant");
+    patternImplementation.register("non-existant", participantC);
+  });
+  it('should wrap with pattern', function() {
+    expect(constructor).toHaveBeenCalledWith("test");
+    expect(patternImplementation.test).toEqual("testing");
+  });
+  it('should create a mediator', function() {
+    expect(Pattern).toBeDefined();
+  });
+  it('should register participants', function() {
+    expect(patternImplementation.count()).toEqual(3);
+  });
+  it('should send messages', function() {
+    expect(participantA).toHaveBeenCalledWith("testA", "participantB");
+    expect(participantB).toHaveBeenCalledWith("testB", "participantA");
+  });
+  it('should be able to broadcast', function() {
+    expect(participantA).toHaveBeenCalledWith("broadcast", "participantA");
+    expect(participantB).toHaveBeenCalledWith("broadcast", "participantA");
+    expect(participantA).toHaveBeenCalledWith("send broadcast", "participantA");
+    expect(participantB).toHaveBeenCalledWith("send broadcast", "participantA");
+  });
+  it('allows sending anonymous messages', function() {
+    expect(participantA).toHaveBeenCalledWith("anonymous", undefined);
+    expect(participantA).toHaveBeenCalledWith("send anonymous broadcast", undefined);
+    expect(participantA).toHaveBeenCalledWith("anonymous broadcast", undefined);
+  });
+  it('should dismiss messages for participants not registered on time', function() {
+    expect(participantC).not.toHaveBeenCalled();
+  });
+});
--- src/patterns/behavioral/mediator.js
+++ src/patterns/behavioral/mediator.js
@@ -2,5 +2,39 @@ import extend from '../../helpers/extend.js';
 import buildPattern from '../../helpers/buildPattern.js';
 
 export default buildPattern(options => {
-  
+  function Mediator(...args) {
+    this.participants = {};
+    options.constructor.apply(this, args);
+  }
+  extend(Mediator.prototype, {
+    register(alias, callback) {
+      if(this.participants[alias] !== undefined) {
+        throw new Error("participant already exists");
+      }
+      this.participants[alias] = callback;
+      return this;
+    },
+
+    send(message, from, to) {
+      if(to === undefined) {
+        return this.broadcast(message, from);
+      }
+      this.participants[to] && this.participants[to](message, from);
+      return this;
+    },
+
+    broadcast(message, from) {
+      for(var prop in this.participants){
+        if(this.participants.hasOwnProperty(prop)){
+          this.participants[prop](message, from);
+        }
+      }
+      return this;
+    },
+
+    count(){
+      return Object.keys(this.participants).length;
+    }
+  });
+  return Mediator;
 });
