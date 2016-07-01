dfea1e41bb81d1d4f8e61885b3ee5f73b99df084
Pablo Garcia <pgarciacamou@gmail.com>
2016-06-30 22:24:05 -0700

    [new pattern] mediator

--- specs/unit/index.spec.js
+++ specs/unit/index.spec.js
@@ -2,12 +2,13 @@ import patterns from '../../src/index.js'
 
 describe('patterns API', function() {
   it('should describe an API', function() {
-    expect(Object.keys(patterns).length).toEqual(4);
+    expect(Object.keys(patterns).length).toEqual(5);
   });
   it('should contain patterns', function() {
     expect(patterns.singleton).toBeDefined();
     expect(patterns.factory).toBeDefined();
     expect(patterns.publishSubscribe).toBeDefined();
     expect(patterns.chainOfResponsibility).toBeDefined();
+    expect(patterns.mediator).toBeDefined();
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import singleton from './patterns/creational/singleton.js';
 import factory from './patterns/creational/factory.js';
 import publishSubscribe from './patterns/behavioral/publishSubscribe.js';
 import chainOfResponsibility from './patterns/behavioral/chainOfResponsibility.js';
+import mediator from './patterns/behavioral/mediator.js';
 
 var patterns = {
   singleton,
   factory,
   publishSubscribe,
-  chainOfResponsibility
+  chainOfResponsibility,
+  mediator
 };
 
 export default patterns;
