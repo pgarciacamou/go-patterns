adb17face8a9f194bd5cdeb860e296d3e4710f22
Pablo Garcia <pgarciacamou@gmail.com>
2016-07-05 09:58:12 -0700

    [new pattern] flyweight

---
+++ src/patterns/structural/flyweight.js
@@ -0,0 +1,9 @@
+import extend from "../../helpers/extend.js";
+import buildPattern from "../../helpers/buildPattern.js";
+
+export default buildPattern(options => {
+  function Flyweight() {
+    this.flyweight = {};
+  }
+  
+});
