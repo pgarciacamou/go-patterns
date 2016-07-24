ba498edbb966f573578667411173ee77ac67e756
Pablo Garcia <pgarciacamou@gmail.com>
2016-07-23 22:53:55 -0700

    added API specs

--- specs/unit/index.spec.js
+++ specs/unit/index.spec.js
@@ -3,7 +3,7 @@ import patterns from '../../index.js';
 
 describe('patterns API', function() {
   it('should describe an API', function() {
-    expect(Object.keys(patterns).length).toEqual(8);
+    expect(Object.keys(patterns).length).toEqual(11);
   });
   it('should contain patterns', function() {
     expect(patterns.singleton).toBeDefined();
@@ -14,5 +14,8 @@ describe('patterns API', function() {
     expect(patterns.command).toBeDefined();
     expect(patterns.memento).toBeDefined();
     expect(patterns.flyweight).toBeDefined();
+    expect(patterns.mvc).toBeDefined();
+    expect(patterns.mvp).toBeDefined();
+    expect(patterns.mvvm).toBeDefined();
   });
 });
