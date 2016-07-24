9bbbae01a0075490e239baf4664748ef67a8bef8
Pablo Garcia <pgarciacamou@gmail.com>
2016-07-23 20:19:15 -0700

    [flyweight test] added mid level tests

--- specs/unit/patterns/structural/flyweight.spec.js
+++ specs/unit/patterns/structural/flyweight.spec.js
@@ -40,4 +40,35 @@ describe('flyweight', function() {
     expect(test.test).toEqual('testing');
     expect(test.test).not.toEqual('already created');
   });
+
+  describe('Mid Level: Memoization with Flyweight', function() {
+    var FactorialMemoizationFlyweight;
+    var factorialMemoizationFlyweight;
+    var factorials;
+    beforeEach(function() {
+      factorials = [2,3,4];
+      FactorialMemoizationFlyweight = singleton(flyweight({
+        publics: {
+          heuristic(val) {
+            return this.flyweights[val+""] = this.flyweights[val] || this.factorial(val);
+          },
+          factorial(val) {
+            if(val <= 1) return 1;
+            return val * this.factorial(val - 1);
+          }
+        }
+      })).build();
+      factorialMemoizationFlyweight = new FactorialMemoizationFlyweight();
+      factorials.forEach(factorialMemoizationFlyweight.create.bind(factorialMemoizationFlyweight));
+    });
+    it('should memoize the values', function() {
+      expect(Object.keys(factorialMemoizationFlyweight.flyweights).length).toEqual(factorials.length);
+    });
+    it('should return memoized values', function() {
+      // we spy on factorial method, AFTER the initial values were declared already.
+      spyOn(factorialMemoizationFlyweight, "factorial");
+      factorials.forEach(factorialMemoizationFlyweight.create.bind(factorialMemoizationFlyweight));
+      expect(factorialMemoizationFlyweight.factorial).not.toHaveBeenCalled();
+    });
+  });
 });
