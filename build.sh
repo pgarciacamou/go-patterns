875f0345d054fbbdff56c5c26d99553cfeab2d47
Pablo Garcia <pgarciacamou@gmail.com>
2017-04-07 13:02:17 -0700

    chage name to go-patterns

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ mkdir dist
 
 MODULE=${MODULE:=4}
 
-# Build patterns.module.min.js
-./node_modules/browserify/bin/cmd.js ./src/index.js -o ./temp/patterns.js -t [ babelify --presets [ es2015 ] ]
-echo "var f = $(cat temp/patterns.js) module.exports = f($MODULE);" > ./temp/patterns.module.js
-node_modules/.bin/uglifyjs --compress --screw-ie-8 --comments --mangle --mangle-props --mangle-regex='/^_/' ./temp/patterns.module.js -o ./dist/patterns.module.js
+# Build go-patterns.module.min.js
+./node_modules/browserify/bin/cmd.js ./src/index.js -o ./temp/go-patterns.js -t [ babelify --presets [ es2015 ] ]
+echo "var f = $(cat temp/go-patterns.js) module.exports = f($MODULE);" > ./temp/go-patterns.module.js
+node_modules/.bin/uglifyjs --compress --screw-ie-8 --comments --mangle --mangle-props --mangle-regex='/^_/' ./temp/go-patterns.module.js -o ./dist/go-patterns.module.js
 
 #Build stand alone version
-./node_modules/browserify/bin/cmd.js ./src/index.js --standalone patterns -o ./dist/patterns.js -t [ babelify --presets [ es2015 ] ]
-./node_modules/.bin/uglifyjs --compress --screw-ie-8 --comments --mangle --mangle-props --mangle-regex='/^_/' ./dist/patterns.js -o ./dist/patterns.min.js
+./node_modules/browserify/bin/cmd.js ./src/index.js --standalone go-patterns -o ./dist/go-patterns.js -t [ babelify --presets [ es2015 ] ]
+./node_modules/.bin/uglifyjs --compress --screw-ie-8 --comments --mangle --mangle-props --mangle-regex='/^_/' ./dist/go-patterns.js -o ./dist/go-patterns.min.js
 
 rm -rf temp
