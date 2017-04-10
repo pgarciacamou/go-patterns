1ea50d021474d2fc8c918d4dc1fb778e95eeb9d5
Pablo Garcia <pgarciacamou@gmail.com>
2017-04-10 12:52:40 -0700

    small refactor

--- specs/unit/patterns/behavioral/memento.spec.js
+++ specs/unit/patterns/behavioral/memento.spec.js
@@ -5,8 +5,8 @@ import chainOfResponsibilityBuilder from '../../../../src/patterns/behavioral/ch
 import json from '../../../../src/helpers/json.js';
 
 describe('memento', function() {
-  var Pattern;
-  var pattern;
+  var Memento;
+  var memento;
   var history;
   var parsedHistory;
   var key;
@@ -16,12 +16,11 @@ describe('memento', function() {
     history = {
       test: 'testing'
     };
-    Pattern = mementoBuilder().build();
-    pattern = new Pattern();
-    key = pattern.add(history);
-    parsedHistory = pattern.get(key);
+    Memento = mementoBuilder().build();
+    memento = new Memento();
+    key = memento.add(history);
+    parsedHistory = memento.get(key);
   });
-
   it('should add an object', function() {
     expect(key).toBeDefined();
     expect(key).toEqual(0);
@@ -32,7 +31,6 @@ describe('memento', function() {
     expect(parsedHistory === history).not.toBeTruthy();
     expect(parsedHistory).toEqual(history);
   });
-
   describe('Advanced Level: Undo Redo using Memento, Command, and ChainOfResponsibility', function() {
     var chain;
     var UndoManager;
