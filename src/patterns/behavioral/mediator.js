6780041a85c0169b43ecaf5d22e1002d1488c5a5
Pablo Garcia <pgarciacamou@gmail.com>
2017-06-25 09:05:45 -0700

    small fix in mediator

--- src/patterns/behavioral/mediator.js
+++ src/patterns/behavioral/mediator.js
@@ -26,7 +26,7 @@ export default createPatternBuilder(options => {
     broadcast(message, from) {
       for(var prop in this.participants) {
         if(this.participants.hasOwnProperty(prop)) {
-          this.participants[prop](message, from);
+          this.participants[prop] && this.participants[prop](message, from);
         }
       }
       return this;
