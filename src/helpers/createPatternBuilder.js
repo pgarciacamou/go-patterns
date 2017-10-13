cde7187c0e053275420c8c1fc019f93ed97059a8
Pablo Garcia <pgarciacamou@gmail.com>
2017-10-12 21:06:01 -0700

    move public props to outer most pattern

--- src/helpers/createPatternBuilder.js
+++ src/helpers/createPatternBuilder.js
@@ -1,29 +1,37 @@
 import extend from './extend.js';
 
-export default function createPatternBuilder(getPattern) {
-  return function(options = {}) {
+export default function createPatternBuilder(getConstructor) {
+  return (options = {}) => {
     options = extend({
+      // can't be an arrow function as it does not have a prototype
       constructor: function() {},
       publics: {},
       statics: {}
     }, options);
 
-    var __class = getPattern(options);
-    __class.prototype = extend(
+    const __constructor = getConstructor(options);
+
+    // half of the dynamic inheritance the other half goes on
+    // within the pattern's constructor itself
+    __constructor.prototype = extend(
       Object.create(options.constructor.prototype),
       { constructor: options.constructor },
-      __class.prototype,
-      options.publics
+      __constructor.prototype
     );
 
-    return {
-      constructor: __class,
-      publics: __class.prototype,
+    const builder = {
+      constructor: __constructor,
+      publics: options.publics,
       statics: options.statics,
       build() {
-        return extend(__class, options.statics);
+        // copy public options into outermost pattern
+        extend(__constructor.prototype, options.publics);
+
+        // extend constructor with static props
+        return extend(__constructor, options.statics);
       }
     };
 
+    return builder;
   };
 }
